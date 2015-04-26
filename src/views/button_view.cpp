879be947bc406c4f3320df6f34fbfe6c7123507d
Tiaan Louw <tiaanl@gmail.com>
2015-04-26 22:18:24 +0200

    Renamed button files.

--- src/views/button_view.cpp
+++ src/views/button_view.cpp
@@ -12,7 +12,7 @@
 // OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
-#include "elastic/views/button.h"
+#include "elastic/views/button_view.h"
 
 #include <cmath>
 #include <algorithm>
