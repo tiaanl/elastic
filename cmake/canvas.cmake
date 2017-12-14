87ba8881219551c65a5b63dbc0399c78f96e6465
Tiaan Louw <tiaanl@gmail.com>
2017-12-14 15:16:53 +0100

    Add cmake include guard

--- cmake/canvas.cmake
+++ cmake/canvas.cmake
@@ -1,7 +1,7 @@
 
-set(CANVAS_ROOT_DIR "" CACHE PATH "Canvas source root directory")
+set(CANVAS_ROOT_DIR "../canvas" CACHE PATH "Canvas source root directory")
 if(NOT CANVAS_ROOT_DIR)
-  message(FATAL_ERROR "CANVAS not found. Please set CANVAS_ROOT_DIR.")
+    message(FATAL_ERROR "CANVAS not found. Please set CANVAS_ROOT_DIR.")
 endif()
 
 add_subdirectory("${CANVAS_ROOT_DIR}" "nucleus")
